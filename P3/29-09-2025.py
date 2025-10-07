98220b25049f35e301572e381714f460c06ae470
IsaacRojasBarrón <isaac.robarron@gmail.com>
2025-10-06 19:39:10 -0600

    Se sube clase 29-09-2025

--- P3/29-09-2025.py
+++ P3/29-09-2025.py
@@ -60,7 +60,61 @@ def desplegar_laberinto (maze, camino = None, considerados = None):
             plt.pause(0.1)
     plt.show()
 
-def A_estrella (maze, punto_inicial, meta):
+def A_estrella(mapa, punto_inicial, meta):
+    lista_abierta = [(punto_inicial, 0, heuristica(punto_inicial, meta), [])] 
+    ## definir a los considerados 
+    filas = np.shape(mapa)[0]
+    columnas = np.shape(mapa)[1]
+    lista_cerrada = np.zeros((filas, columnas))
+    ## A traves de la varible considerados vamos a ir guardando los nodos que ya hemos visitado y el camino que se irá construyendo
+    considerados = []
+    while len(lista_abierta) > 0:
+        menor_f = lista_abierta[0][2]
+        nodo_actual,g_actual, f_actual, camino = lista_abierta[0]
+
+        ##evaluar los nodos vecinos/hijos
+        indice_menor_f = 0
+        for i in range(1, len(lista_abierta)):
+            ##Extraemos F del nodo a evaluar y lo comparamos con el menor F_actual
+            if lista_abierta[i][2] < f_actual:
+                menor_f = lista_abierta[i][2]
+                nodo_actual, g_actual, f_actual, camino = lista_abierta[i]
+                indice_menor_f = i
+        ##Eliminar el nodo actual de la lista abierta
+        lista_abierta = lista_abierta[:indice_menor_f] + lista_abierta[indice_menor_f+1:]
+        ## guardar en considerados el nodo actual
+        considerados += [nodo_actual]
+
+
+        ##Evaluamos si es la meta
+        if nodo_actual == meta:
+            return camino + [nodo_actual], considerados
+        ##ya visitamos el nodo actual
+        lista_cerrada[nodo_actual[0], nodo_actual[1]]=1
+
+        for vecinos in movimientos:
+            vecino = (nodo_actual[0] + vecinos[0], nodo_actual[1] + vecinos[1])
+            ##Corroborar que el vecino esté dentro del mapa y que el vecino sea un nodo transitable y que no haya sido visitado ese nodo previamente
+            if ((0 <= vecino[0] < filas) and (0 <= vecino[1] < columnas) and (mapa[vecino[0], vecino[1]] == 0) and (lista_cerrada[vecino[0], vecino[1]] == 0)):
+            ##Calcular el valor de la g nuevas, evaluando si es vertical/horizontal o diagonal
+                if (abs(vecinos[0]) + abs(vecinos[1])) == 2:
+                    g_nuevo = g_actual + 14
+                else:
+                    g_nuevo = g_actual + 10
+                f_nuevo = g_nuevo + heuristica(vecino, meta)
+
+            ##verificar si el vecino ya está en la lista abierta
+                banderita_lista_abierta = False
+                for nodo, g, f, camino_tmp in lista_abierta:
+                    if nodo == vecino and f <= f_nuevo:
+                        banderita_lista_abierta = True
+                        break
+
+                if banderita_lista_abierta == False:
+                    # agrega el camino extendiendo con el nodo actual (no el vecino)
+                    lista_abierta += [(vecino, g_nuevo, f_nuevo, camino + [nodo_actual])]
+
+
     ##(Punto_inicial,g , f, camino)
     lista_abierta = [(punto_inicial,0,heuristica(punto_inicial,meta),[])]
     print(len(lista_abierta))
