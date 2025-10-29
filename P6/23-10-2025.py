410e4cac0cb92872345b440a74684859c046dece
IsaacRojasBarrón <isaac.robarron@gmail.com>
2025-10-29 01:11:46 -0600

    Actualización.

--- P6/23-10-2025.py
+++ P6/23-10-2025.py
@@ -13,36 +13,36 @@ epsilon = 0.000001
 epocas_max = 20000
 epochs = 0
 
-def regresion_ride (X, Yr, lr,T,epsilon, epocas_max):
-    ## Número de muestras
-    m = len(X)
-    #Ym = np.zeros(m)
 
-    ## Inicializacion de coeficientes Beta
-    b0 = -(np.random.random()) ##Se recomienda que b0 empieze en negativos
-    b1 = np.random.random()
+## Número de muestras
+m = len(X)
+#Ym = np.zeros(m)
 
-    J = 10 ##va a calcular j en cada epoca
-    Jant = 0
-    ECM = [] ##Este lo guarda
+## Inicializacion de coeficientes Beta
+b0 = -(np.random.random()) ##Se recomienda que b0 empieze en negativos
+b1 = np.random.random()
+
+J = 10 ##va a calcular j en cada epoca
+Jant = 0
+ECM = [] ##Este lo guarda
 
 
-    ##########################Fase de entrenamiento#################################
-    while (epochs <= epocas_max and abs(J - Jant) > epsilon):
+##########################Fase de entrenamiento#################################
+while (epochs <= epocas_max and abs(J - Jant) > epsilon):
 
-        Ym = b0 + b1*X
-        Jant = J
+    Ym = b0 + b1*X
+    Jant = J
 
-        J = (1/(2*m))*np.sum((Yr-Ym)**2) + T*b1**2
-        ECM += [J]
+    J = (1/(2*m))*np.sum((Yr-Ym)**2) + T*b1**2
+    ECM += [J]
 
-        ##Descenso de gradiente
-        b0 = b0 - (lr/m)*np.sum(Ym-Yr)
-        b1 = b1 - (lr/m)*np.dot((Ym-Yr), X) + T*b1
+    ##Descenso de gradiente
+    b0 = b0 - (lr/m)*np.sum(Ym-Yr)
+    b1 = b1 - (lr/m)*np.dot((Ym-Yr), X) + T*b1
 
-        epochs = epochs + 1
-        print(f"Época Número: {epochs}")
-        print(f"El valor de b0 es igual a: {b0} y el de b1 {b1}")
+    epochs = epochs + 1
+    print(f"Época Número: {epochs}")
+    print(f"El valor de b0 es igual a: {b0} y el de b1 {b1}")
 
-    plt.plot(ECM)
-    ###################################################################################
+plt.plot(ECM)
+###################################################################################
